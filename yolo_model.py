96d5e23f680af19d3b643c7bb25ee982a4ad91cb
vittorio.picone <vittorio.picone@greentechsolution.it>
2024-07-12 13:06:28 +0200

    Train code and dataset provided

--- yolo_model.py
+++ yolo_model.py
@@ -1,25 +1,5 @@
-import json
-import logging
 import os
-import shutil
-import sys
-from collections import defaultdict
-from itertools import product
-from typing import Union
-import cv2
-import numpy
-import numpy as np
-from skimage.morphology import medial_axis
-from tqdm import tqdm
 from ultralytics import YOLO
-import math as m
-from openpyxl import load_workbook
-import common
-
-ROOT_DIR = os.path.abspath(os.getcwd())
-sys.path.append(ROOT_DIR)
-TEMPLATE_REPORT = os.path.join(ROOT_DIR, 'template_report.xlsx')
-
 
 def predict(model_path: str, img_path: str):
     # Load a model
@@ -43,8 +23,10 @@ def train( dataset_dir: str, num_epochs: int, imgsz: int, model_path: str = "yol
         raise FileNotFoundError(f"Model file not found at {model_path}")
 
     model = YOLO(model_path)  # load a pretrained model (recommended for training)
-    results = model.train(data=dataset_dir, epochs=num_epochs, imgsz=imgsz, patience=0, weight_decay=l2_lambda)
+    results = model.train(data=dataset_dir, epochs=num_epochs, imgsz=imgsz, patience=0, batch=4, weight_decay=l2_lambda)
     print(results)
 
 if __name__ == '__main__':
-    train("datasets/wall/v4/data.yaml", 150, 640)
+    train("dataset/data.yaml", 20, 640)
+    #predict("runs/segment/train/weights/best.pt", "test.jpg")
+
